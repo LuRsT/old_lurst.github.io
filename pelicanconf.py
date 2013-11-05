b365bfd5aa6964eebb295d2a7bed0143e3fbbf86
Gilberto Gonçalves <lursty@gmail.com>
2013-11-05 23:34:17 +0000

    Using STATIC_PATHS instead of FILES_TO_COPY

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,8 +30,7 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-FILES_TO_COPY = (
-    ('extra/CNAME', 'CNAME'),
-)
+STATIC_PATHS = ['extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 
 THEME = "theme"
