3365a8bbef0bfc9830dbdc3ce52e3a4f96b91c69
Gilberto Gonçalves <lursty@gmail.com>
2013-11-06 10:30:44 +0000

    Changed site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://lurst.github.io/'
+SITEURL = 'http://lumbercoder.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
